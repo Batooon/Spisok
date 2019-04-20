f7935f10e769b07196dcb2d9f0ec94f13d1de832
Batooon <rozum.ant@gmail.com>
2019-04-20 17:52:14 +0300

    Added class node and list

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spisok
+{
+    class Node
+    {
+        Node next;
+        public int data;
+        public void SetNextNode(Node nextNode)
+        {
+            next = nextNode;
+        }
+
+    }
+}
---
+++ SList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spisok
+{
+    class SList
+    {
+        Node head;
+        Node tail;
+        int Length;
+        public SList()
+        {
+            Length = 0;
+            head = null;
+            tail = head;
+        }
+        public void Push(int value)
+        {
+            if (head == null)
+            {
+                head = new Node();
+                head.data = value;
+                tail = head;
+                head.SetNextNode(null);
+            }
+            else
+            {
+                Node newNode = new Node();
+                tail.SetNextNode(newNode);
+                tail = newNode;
+                tail.data = value;
+                tail.SetNextNode(null);
+            }
+            Length++;
+        }
+        public void Pop()
+        {
+
+        }
+        public int Spisoklength()
+        {
+            return Length;
+        }
+    }
+}
